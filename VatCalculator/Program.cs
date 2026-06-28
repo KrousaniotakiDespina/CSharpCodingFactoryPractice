01821e1b47ee194da798198503137d3c8b88bf8a
KrousaniotakiDespina <despoinakrr@gmail.com>
2026-06-28 19:16:56 +0300

    chore: add VatCalculator

---
+++ VatCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace VatCalculator
+{
+
+    /// <summary>
+    /// Reads a product price from the user, validates the input and calculates the VAT and total price based on a given VAT rate.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaration and initialization of variables
+            const double VAT_RATE = 0.24;
+            double price = 0D;
+            double vat = 0D;
+            double total = 0D;
+
+            // Data input and validation
+            Console.WriteLine("Παρακαλώ δώστε την τιμή:");
+            if(!double.TryParse(Console.ReadLine(), out price) || price < 0)
+            {
+                Console.WriteLine("Μη έγκυρος αριθμός.");
+                return;
+            }
+
+            // Calculations
+            vat = price * VAT_RATE;
+            total = price + vat;
+
+            // Output
+            Console.WriteLine($"{"Τιμή χωρίς ΦΠΑ:"} {price:N2} €");
+            Console.WriteLine($"{"ΦΠΑ 24%:"} {vat:N2} €");
+            Console.WriteLine($"{"Τιμή με ΦΠΑ:"} {total:N2} €");
+        }
+    }
+}
